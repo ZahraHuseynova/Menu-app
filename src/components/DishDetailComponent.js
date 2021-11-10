9ca93f730d88634c0dddcf1ed37837f7b04361a4
Zahra Huseynova <zahra.huseynova@gmail.com>
2021-11-10 15:26:29 +0400

    post comment

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -29,7 +29,7 @@ class CommentForm extends Component{
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.dishId,values.rating,values.author,values.comment)
+        this.props.postComment(this.props.dishId,values.rating,values.author,values.comment)
         
     }
 
@@ -116,7 +116,7 @@ class CommentForm extends Component{
             )   
     }
 
-    function RenderComments({comments,addComment,dishId}){
+    function RenderComments({comments,postComment,dishId}){
         if(comments !=null){
             const commentListItem = comments.map((comment)=>{
 
@@ -137,7 +137,7 @@ class CommentForm extends Component{
                         {commentListItem}
 
                     </ul>
-                    <CommentForm dishId={dishId} addComment={addComment}/>
+                    <CommentForm dishId={dishId} postComment={postComment}/>
                 </div>
             )
             
@@ -185,7 +185,7 @@ class CommentForm extends Component{
                     <div className="row">
                         <RenderDish dish={props.dish}/>
                         <RenderComments comments={props.comments}
-                            addComment={props.addComment}
+                            postComment={props.postComment}
                             dishId={props.dish.id}/>
                         
                     </div>
