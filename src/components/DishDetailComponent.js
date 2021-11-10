141701c50f3a7b729c4711825a998e094ce3c831
Zahra Huseynova <zahra.huseynova@gmail.com>
2021-11-10 14:03:21 +0400

    fetch from server

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -3,6 +3,7 @@ import { Card,CardImg,CardText,CardBody,CardTitle,BreadcrumbItem,Breadcrumb,Butt
 import {Link} from "react-router-dom"
 import {Control,LocalForm,Errors} from "react-redux-form"
 import { Loading } from "./LoadingComponent";
+import { baseUrl } from "../shared/baseUrl";
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -105,7 +106,7 @@ class CommentForm extends Component{
             return (
                     <div className="col-12 col-md-5 m-1">
                         <Card>
-                            <CardImg src={dish.image} alt={dish.name} />
+                            <CardImg src={baseUrl + dish.image} alt={dish.name} />
                             <CardBody>
                             <CardTitle>{dish.name}</CardTitle>
                             <CardText>{dish.description}</CardText>
